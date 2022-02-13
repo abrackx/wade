329f191c0dce9dfc25966b4620d55cce8c41e96d
abrackx <abrackx@gmail.com>
2022-02-12 22:58:57 -0500

    removes unnecessary fn

--- src/stomp/frame.rs
+++ src/stomp/frame.rs
@@ -1,3 +1,4 @@
+use std::ascii::escape_default;
 use crate::stomp::command::Command;
 
 pub struct Frame {
@@ -7,42 +8,20 @@ pub struct Frame {
 }
 
 pub fn serialize(frame: Frame) -> Vec<u8> {
-    fn write_escaped(byte: u8, buffer: &mut Vec<u8>) {
-        match byte {
-            b'\r' => {
-                buffer.push(b'\\');
-                buffer.push(b'r')
-            }
-            b'\n' => {
-                buffer.push(b'\\');
-                buffer.push(b'n')
-            }
-            b':' => {
-                buffer.push(b'\\');
-                buffer.push(b'c')
-            }
-            b'\\' => {
-                buffer.push(b'\\');
-                buffer.push(b'\\')
-            }
-            byte => buffer.push(byte),
-        }
-    }
     let mut buffer = vec![];
     buffer.extend_from_slice(frame.command.to_string().as_bytes());
     buffer.push(b'\n');
     frame.headers.iter().for_each(|(key, val)| {
         for byte in key.as_bytes() {
-            write_escaped(*byte, &mut buffer);
+            buffer.extend_from_slice(escape_default(*byte).to_string().as_bytes());
         }
         buffer.push(b':');
         for byte in val.as_bytes().iter() {
-            write_escaped(*byte, &mut buffer);
+            buffer.extend_from_slice(escape_default(*byte).to_string().as_bytes());
         }
         buffer.push(b'\n');
     });
     if let Some(body) = frame.body {
-        //todo: add content length header
         buffer.push(b'\n');
         buffer.extend_from_slice(body.as_bytes());
     } else {
@@ -89,11 +68,13 @@ passcode:password\n\n\x00"
         let data = b"CONNECT
 foo:foo
 accept-version:1.2
+content-length:2
 host:example.com\n\n\x00"
             .to_vec();
         let headers = vec![
             ("foo".to_string(), "foo".to_string()),
             ("accept-version".to_string(), "1.2".to_string()),
+            ("content-length".to_string(), "2".to_string()),
             ("host".to_string(), "example.com".to_string()),
         ];
         let frame = Frame {
